5ecc787bdf0ff60c2f0ad3b0cc3974968c92a9b5
John Shannon <jiang.h@ikcrm.com>
2020-02-03 17:12:12 +0800

    epand data path depend on relative file path

--- lib/given.rb
+++ lib/given.rb
@@ -2,20 +2,21 @@ require 'given/random'
 require 'given/full_name'
 
 module Given
+  data_dir =  File.expand_path('../../data', __FILE__)
 
-  File.open("./data/chinese_family_names.txt", 'r') do |file|
+  File.open("#{data_dir}/chinese_family_names.txt", 'r') do |file|
     CHINESE_FAMILY_NAMES = file.map{|line| line.strip}
   end
 
-  File.open("./data/chinese_given_names.txt", 'r') do |file|
+  File.open("#{data_dir}/chinese_given_names.txt", 'r') do |file|
     CHINESE_GIVEN_NAMES = file.map{|line| line.strip}
   end
 
-  File.open("./data/latin_family_names.txt", 'r') do |file|
+  File.open("#{data_dir}/latin_family_names.txt", 'r') do |file|
     LATIN_FAMILY_NAMES = file.map{|line| line.strip}
   end
 
-  File.open("./data/latin_given_names.txt", 'r') do |file|
+  File.open("#{data_dir}/latin_given_names.txt", 'r') do |file|
     LATIN_GIVEN_NAMES = file.map{|line| line.strip}
   end
 
